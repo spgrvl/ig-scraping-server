0c58ed1f9b388c603bfe9c2ccf558e3345341de1
Spyros <spgrvl@gmail.com>
2020-10-17 21:38:51 +0300

    Optimized code for faster load of post data.

--- server.py
+++ server.py
@@ -15,6 +15,7 @@ import logging
 logging.basicConfig(filename='error.log',level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler())
 
+# Set chromedriver path based on os
 if os.name == "nt":
 	chrome_path = r"C:\chromedriver_win32\chromedriver.exe"
 elif os.name == "posix":
@@ -22,40 +23,33 @@ elif os.name == "posix":
 else:
 	print("Error: Chrome path is not set for this os: " + os.name)
 
+# Open headless chromedriver
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(chrome_path,chrome_options=chrome_options)
 
-def views_count(shortcode):
+def get_post_data(shortcode):
 	url = "https://www.instagram.com/p/" + shortcode
 	driver.get(url)
 	if (driver.title == "Page Not Found • Instagram"):
 		return -2
+	page_source = driver.page_source
 	try:
-		page_source = driver.page_source
-		views_count = int(re.findall('(?<="video_view_count":)[0-9]*(?=,)', page_source)[0])
-		return views_count
+		likes_count = re.findall('(?<="edge_media_preview_like":{"count":)[0-9]*(?=,)', page_source)[0]
 	except:
-		return -1
-
-def likes_count(shortcode):
-	url = "https://www.instagram.com/p/" + shortcode
-	driver.get(url)
-	if (driver.title == "Page Not Found • Instagram"):
-		return -2
+		likes_count = -1
 	try:
-		page_source = driver.page_source
-		likes_count = int(re.findall('(?<="edge_media_preview_like":{"count":)[0-9]*(?=,)', page_source)[0])
-		return likes_count
+		views_count = re.findall('(?<="video_view_count":)[0-9]*(?=,)', page_source)[0]
 	except:
-		return -1
+		views_count = -1
+	return likes_count, views_count
 
-def followers_count(username):
+def get_user_data(username):
 	url = "https://www.instagram.com/" + username + "?__a=1"
 	driver.get(url)
+	page_source = driver.page_source
 	try:
-		page_source = driver.page_source
-		followers_count = int(re.findall('(?<="edge_followed_by":{"count":)[0-9]*(?=})', page_source)[0])
+		followers_count = re.findall('(?<="edge_followed_by":{"count":)[0-9]*(?=})', page_source)[0]
 		return followers_count
 	except:
 		return -1
@@ -66,15 +60,19 @@ def favicon():
 
 @app.route("/p/<shortcode>/", methods=['GET'])
 def api_post(shortcode):
-	likes_counter = str(likes_count(shortcode))
-	views_counter = str(views_count(shortcode))
-	data = {'shortcode':shortcode, 'likes_count':likes_counter, 'views_count':views_counter}
+	post_data = get_post_data(shortcode)
+	if type(post_data) == int:
+		likes_counter = views_counter = post_data
+	else:
+		likes_counter = post_data[0]
+		views_counter = post_data[1]
+	data = {'shortcode':str(shortcode), 'likes_count':str(likes_counter), 'views_count':str(views_counter)}
 	response = app.response_class(response=json.dumps(data), mimetype='application/json')
 	return response
 
 @app.route("/<username>/", methods=['GET'])
 def api_user(username):
-	followers_counter = str(followers_count(username))
+	followers_counter = get_user_data(username)
 	data = {'username':username, 'followers_count':str(followers_counter)}
 	response = app.response_class(response=json.dumps(data), mimetype='application/json')
 	return response
