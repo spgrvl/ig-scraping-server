7499b795ab24a701936c1d3db171371e1e25922b
Spyros <spgrvl@gmail.com>
2020-12-08 11:53:14 +0200

    Fixed indentation issues.

--- server.py
+++ server.py
@@ -17,11 +17,11 @@ logging.getLogger().addHandler(logging.StreamHandler())
 
 # Set chromedriver path based on os
 if os.name == "nt":
-	chrome_path = r"C:\chromedriver_win32\chromedriver.exe"
+    chrome_path = r"C:\chromedriver_win32\chromedriver.exe"
 elif os.name == "posix":
-	chrome_path = "/usr/lib/chromium-browser/chromedriver"
+    chrome_path = "/usr/lib/chromium-browser/chromedriver"
 else:
-	print("Error: Chrome path is not set for this os: " + os.name)
+    print("Error: Chrome path is not set for this os: " + os.name)
 
 # Open headless chromedriver
 chrome_options = Options()
@@ -29,68 +29,68 @@ chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(chrome_path,chrome_options=chrome_options)
 
 def get_post_data(shortcode):
-	url = "https://www.instagram.com/p/" + shortcode
-	driver.get(url)
-	if (driver.title == "Page Not Found • Instagram"):
-		return -2
-	page_source = driver.page_source
-	try:
-		likes_count = re.findall('(?<="edge_media_preview_like":{"count":)[0-9]*(?=,)', page_source)[0]
-	except:
-		likes_count = -1
-	try:
-		views_count = re.findall('(?<="video_view_count":)[0-9]*(?=,)', page_source)[0]
-	except:
-		views_count = -1
-	return likes_count, views_count
+    url = "https://www.instagram.com/p/" + shortcode
+    driver.get(url)
+    if (driver.title == "Page Not Found • Instagram"):
+        return -2
+    page_source = driver.page_source
+    try:
+        likes_count = re.findall('(?<="edge_media_preview_like":{"count":)[0-9]*(?=,)', page_source)[0]
+    except:
+        likes_count = -1
+    try:
+        views_count = re.findall('(?<="video_view_count":)[0-9]*(?=,)', page_source)[0]
+    except:
+        views_count = -1
+    return likes_count, views_count
 
 def get_user_data(username):
-	url = "https://www.instagram.com/" + username + "?__a=1"
-	driver.get(url)
-	pre = driver.find_element_by_tag_name("pre").text
-	json_data = json.loads(pre)
-	if json_data == {}:
-		return -2, -2, -2, -2
-	try:
-		followers_count = json_data['graphql']['user']['edge_followed_by']['count']
-		following_count = json_data['graphql']['user']['edge_follow']['count']
-		posts_count = json_data['graphql']['user']['edge_owner_to_timeline_media']['count']
-		is_private = json_data['graphql']['user']['is_private']
-		return followers_count, following_count, posts_count, is_private
-	except:
-		return -1, -1, -1, -1
+    url = "https://www.instagram.com/" + username + "?__a=1"
+    driver.get(url)
+    pre = driver.find_element_by_tag_name("pre").text
+    json_data = json.loads(pre)
+    if json_data == {}:
+        return -2, -2, -2, -2
+    try:
+        followers_count = json_data['graphql']['user']['edge_followed_by']['count']
+        following_count = json_data['graphql']['user']['edge_follow']['count']
+        posts_count = json_data['graphql']['user']['edge_owner_to_timeline_media']['count']
+        is_private = json_data['graphql']['user']['is_private']
+        return followers_count, following_count, posts_count, is_private
+    except:
+        return -1, -1, -1, -1
 
 @app.route('/favicon.ico')
 def favicon():
-	return send_file('favicon.ico')
+    return send_file('favicon.ico')
 
 @app.route("/p/<shortcode>/", methods=['GET'])
 def api_post(shortcode):
-	post_data = get_post_data(shortcode)
-	if type(post_data) == int:
-		likes_counter = views_counter = post_data
-	else:
-		likes_counter = post_data[0]
-		views_counter = post_data[1]
-	data = {'shortcode':str(shortcode), 'likes_count':str(likes_counter), 'views_count':str(views_counter)}
-	response = app.response_class(response=json.dumps(data), mimetype='application/json')
-	return response
+    post_data = get_post_data(shortcode)
+    if type(post_data) == int:
+        likes_counter = views_counter = post_data
+    else:
+        likes_counter = post_data[0]
+        views_counter = post_data[1]
+    data = {'shortcode':str(shortcode), 'likes_count':str(likes_counter), 'views_count':str(views_counter)}
+    response = app.response_class(response=json.dumps(data), mimetype='application/json')
+    return response
 
 @app.route("/<username>/", methods=['GET'])
 def api_user(username):
-	user_data = get_user_data(username)
-	followers_counter = user_data[0]
-	following_counter = user_data[1]
-	posts_counter = user_data[2]
-	is_private = user_data[3]
-	data = {'username':username, 'followers_count':str(followers_counter), 'following_count':str(following_counter), 'posts_count':str(posts_counter), 'is_private':str(is_private)}
-	response = app.response_class(response=json.dumps(data), mimetype='application/json')
-	return response
+    user_data = get_user_data(username)
+    followers_counter = user_data[0]
+    following_counter = user_data[1]
+    posts_counter = user_data[2]
+    is_private = user_data[3]
+    data = {'username':username, 'followers_count':str(followers_counter), 'following_count':str(following_counter), 'posts_count':str(posts_counter), 'is_private':str(is_private)}
+    response = app.response_class(response=json.dumps(data), mimetype='application/json')
+    return response
 
 @app.errorhandler(404)
 def page_not_found(error):
-	data = {'message':'not found'}
-	response = app.response_class(response=json.dumps(data), status=404, mimetype='application/json')
-	return response
+    data = {'message':'not found'}
+    response = app.response_class(response=json.dumps(data), status=404, mimetype='application/json')
+    return response
 
 app.run(host='0.0.0.0', port=18450)
